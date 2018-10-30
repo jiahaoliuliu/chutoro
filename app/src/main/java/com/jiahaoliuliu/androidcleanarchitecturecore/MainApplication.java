d52c745cf841c5ef252ac4bf923dfaf75100d88c
Jiahao Liu <jiahao.liu@dnata.com>
2018-10-30 15:11:57 +0400

    Adding dagger to the libraries

---
+++ app/src/main/java/com/jiahaoliuliu/androidcleanarchitecturecore/MainApplication.java
@@ -0,0 +1,11 @@
+package com.jiahaoliuliu.androidcleanarchitecturecore;
+
+import android.app.Application;
+
+public class MainApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
---
+++ app/src/main/java/com/jiahaoliuliu/androidcleanarchitecturecore/movieslist/MovilesListModule.java
@@ -0,0 +1,7 @@
+package com.jiahaoliuliu.androidcleanarchitecturecore.movieslist;
+
+import dagger.Module;
+
+@Module
+public class MovilesListModule {
+}
