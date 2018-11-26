913c92e9102db64092b873bd05fd7c93ad3bda64
Jiahao Liu <jiahaoliuliu@gmail.com>
2018-11-26 20:22:54 +0400

    Deactivated the network layer

--- datalayer/src/main/java/com/jiahaoliuliu/chutoro/datalayer/itemsrepository/ItemsRepositoryImpl.java
+++ datalayer/src/main/java/com/jiahaoliuliu/chutoro/datalayer/itemsrepository/ItemsRepositoryImpl.java
@@ -3,7 +3,7 @@ package com.jiahaoliuliu.chutoro.datalayer.itemsrepository;
 import android.util.Log;
 
 import com.jiahaoliuliu.chutoro.entity.Item;
-import com.jiahaoliuliu.chutoro.networklayer.ItemService;
+//import com.jiahaoliuliu.chutoro.networklayer.ItemService;
 import com.jiahaoliuliu.chutoro.storagelayer.ItemImpl;
 import com.jiahaoliuliu.chutoro.storagelayer.ItemsDatabase;
 
@@ -16,44 +16,48 @@ public class ItemsRepositoryImpl implements ItemsRepository {
 
     private static final String TAG = "ItemsRepositoryImpl";
 
-    private final ItemService tmdbService;
+//    private final ItemService itemService;
     private final ItemsDatabase itemsDatabase;
     // Temporal memory for the items list
     private List<? extends Item> memoryCache = new ArrayList<>();
 
-    public ItemsRepositoryImpl(ItemService tmdbService, ItemsDatabase itemsDatabase) {
-        this.tmdbService = tmdbService;
+    public ItemsRepositoryImpl(
+//            ItemService itemService,
+            ItemsDatabase itemsDatabase) {
+//        this.itemService = itemService;
         this.itemsDatabase = itemsDatabase;
     }
 
     @Override
     public Single<? extends List<? extends Item>> retrieveItemsList() {
         // List of Priorities
-        Single<? extends List<? extends Item>> backendSource = retrieveItemsListFromBackend();
+//        Single<? extends List<? extends Item>> backendSource = retrieveItemsListFromBackend();
         Single<? extends List<? extends Item>> storageSource = retrieveItemsListFromStorage();
         Single<? extends List<? extends Item>> cacheSource = retrieveItemsListFromCache();
 
-        return Single.concat(backendSource, storageSource, cacheSource)
+        return Single.concat(
+//                backendSource,
+                storageSource, cacheSource)
                 .filter(source -> !source.isEmpty())
                 .first(memoryCache);
     }
 
-    private Single<? extends List<? extends Item>> retrieveItemsListFromBackend() {
-        return tmdbService.getItemsList()
-             .map(itemsListBackend -> itemsListBackend.getItemsList())
-             .doOnSuccess(itemsList -> {
-                // Updates the internal cache
-                saveItemsListToCache(itemsList);
-                // Save the content into the database
-                for (Item item: itemsList) {
-                    Log.v(TAG, "Trying to save " + item + " into the database");
-                    itemsDatabase.itemDao().upsert(new ItemImpl(item));
-                }
-             }).onErrorResumeNext(throwable -> {
-                 Log.e(TAG, "Error retrieving data from backend", throwable);
-                 return Single.just(new ArrayList<>());
-             });
-    }
+//    private Single<? extends List<? extends Item>> retrieveItemsListFromBackend() {
+//        return tmdbService.getItemsList()
+//             .map(itemsListBackend -> itemsListBackend.getItemsList())
+//             .doOnSuccess(itemsList -> {
+//                // Updates the internal cache
+//                saveItemsListToCache(itemsList);
+//                // Save the content into the database
+//                for (Item item: itemsList) {
+//                    Log.v(TAG, "Trying to save " + item + " into the database");
+//                    itemsDatabase.itemDao().upsert(new ItemImpl(item));
+//                }
+//             }).onErrorResumeNext(throwable -> {
+//                 Log.e(TAG, "Error retrieving data from backend", throwable);
+//                 return Single.just(new ArrayList<>());
+//             });
+//    }
 
     private Single<? extends List<? extends Item>> retrieveItemsListFromCache() {
         return Single.just(memoryCache);
