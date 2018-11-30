ae3310da54c8a8fd3bb861096bf7f67514105f97
Jiahao Liu <jiahaoliuliu@gmail.com>
2018-11-30 12:31:01 +0400

    Added a failure test

--- devicelayer/src/test/java/com/jiahaoliuliu/chutoro/devicelayer/smsparser/SmsParserHelperTest.java
+++ devicelayer/src/test/java/com/jiahaoliuliu/chutoro/devicelayer/smsparser/SmsParserHelperTest.java
@@ -16,14 +16,20 @@ import static org.junit.Assert.assertEquals;
 @RunWith(MockitoJUnitRunner.class)
 public class SmsParserHelperTest {
 
+    private static final String PATTERN_1 = "Your credit card (.*?) was used for AED(.*?) on (.*?) at (.*?)\\. ";
+
     private static final long SMS_1_ID = 6543;
     private static final int SMS_1_QUANTITY = 359;
     private static final long SMS_1_DATE = 1542443169000l;
     private static final String SMS_1_DESTINATION = "GOOGLE*IDREAMS,G.CO HELPPAY#-US";
     private static final String SMS_1_BODY = "Your credit card XXX4921 was used for AED3.59 on 17/11/2018 12:26:09 at "
+            + SMS_1_DESTINATION + ". Available credit limit is now AED9192.92.";
+
+    private static final long SMS_2_ID = 6542;
+    private static final long SMS_2_DATE = 1542443169000l;
+    private static final String SMS_2_BODY = "Your credit card XXX4921 was used for Extra word AED3.59 on 17/11/2018 12:26:09 at "
             + SMS_1_DESTINATION + ". Available credit limit is now AED39192.92.";
 
-    private static final String PATTERN_1 = "Your credit card (.*?) was used for AED(.*?) on (.*?) at (.*?)\\. ";
     private static final String DATE_FORMAT = "dd/MM/yyyy HH:mm:ss";
     private static final int POSITION_QUANTITY = 2;
     private static final int POSITION_DESTINATION = 4;
@@ -54,5 +60,22 @@ public class SmsParserHelperTest {
         // Verify the results
         assertEquals(1, transactionList.size());
         assertEquals(rightTransaction, transactionList.get(0));
+    }
+
+    @Test
+    public void mapSmsListToTransactionsList_Failure() {
+        // Prepare the data
+        Sms sms = new Sms(SMS_2_ID, SMS_2_BODY, SMS_2_DATE);
+        List<Sms> smsList = new ArrayList();
+        smsList.add(sms);
+
+        SmsParserParameters smsParserParameters = new SmsParserParameters(PATTERN_1, DATE_FORMAT,
+                POSITION_QUANTITY, POSITION_DESTINATION, POSITION_DATE, SOURCE);
+
+        // Execute the method
+        List<ITransaction> transactionList = smsParserHelper.mapSmsListToTransactionsList(smsList, smsParserParameters);
+
+        // Verify the results
+        assertEquals(0, transactionList.size());
     }
 }
