3959bd64c71dfbf340e106fa3717900a8b883c79
Jiahao Liu <jiahaoliuliu@gmail.com>
2018-12-01 20:32:55 +0400

    Added the second pattern on the ADCB. Added unit tests specially for the patterns

--- devicelayer/src/main/java/com/jiahaoliuliu/chutoro/devicelayer/smsparser/smsparserparameters/ADCBSmsParserParametersFactory.java
+++ devicelayer/src/main/java/com/jiahaoliuliu/chutoro/devicelayer/smsparser/smsparserparameters/ADCBSmsParserParametersFactory.java
@@ -7,12 +7,21 @@ public class ADCBSmsParserParametersFactory implements ISmsParserParametersFacto
 
     private static final String SOURCE = "ADCB";
 
-    // Pattern parameters
+    // Pattern 1
+    // Example Your credit card XXX4921 was used for USD15.00 on 20/11/2018 14:31:31 at PREY, INC.,SAN FRANCISCO-US. Available credit limit is now AED39109.37.
     private static final String PATTERN_1 = "Your credit card (.*?) was used for AED(.*?) on (.*?) at (.*?)\\. ";
     private static final String PATTERN_1_DATE_FORMAT = "dd/MM/yyyy HH:mm:ss";
     private static final int PATTERN_1_POSITION_QUANTITY = 2;
     private static final int PATTERN_1_POSITION_DESTINATION = 4;
     private static final int PATTERN_1_POSITION_DATE = 3;
+
+    // Pattern 2
+    // Example: AED1200.00 withdrawn from acc. XXX132001 on Nov 16 2018  9:40AM at ATM-MARINA MALL DXB. Avl.Bal.AED29349.34. Be cautious with large amt. of cash.
+    private static final String PATTERN_2 = "AED(.*?) withdrawn from acc.(.*?) on (.*?) at (.*?)\\.";
+    private static final String PATTERN_2_DATE_FORMAT = "MMM dd yyyy  hh:mmaa";
+    private static final int PATTERN_2_POSITION_QUANTITY = 1;
+    private static final int PATTERN_2_POSITION_DESTINATION = 4;
+    private static final int PATTERN_2_POSITION_DATE = 3;
 
     @Override
     public List<SmsParserParameters> createSmsParserParametersList() {
@@ -20,6 +29,10 @@ public class ADCBSmsParserParametersFactory implements ISmsParserParametersFacto
                 PATTERN_1, PATTERN_1_DATE_FORMAT, PATTERN_1_POSITION_QUANTITY,
                 PATTERN_1_POSITION_DESTINATION, PATTERN_1_POSITION_DATE, SOURCE);
 
-        return Arrays.asList(smsParserParameters1);
+        SmsParserParameters smsParserParameters2 = new SmsParserParameters(
+                PATTERN_2, PATTERN_2_DATE_FORMAT, PATTERN_2_POSITION_QUANTITY,
+                PATTERN_2_POSITION_DESTINATION, PATTERN_2_POSITION_DATE, SOURCE);
+
+        return Arrays.asList(smsParserParameters1, smsParserParameters2);
     }
 }
---
+++ devicelayer/src/test/java/com/jiahaoliuliu/chutoro/devicelayer/smsparser/smsparserparameters/ADCBSmsParserParametersFactoryTest.java
@@ -0,0 +1,87 @@
+package com.jiahaoliuliu.chutoro.devicelayer.smsparser.smsparserparameters;
+
+import com.jiahaoliuliu.chutoro.devicelayer.smsparser.Sms;
+import com.jiahaoliuliu.chutoro.devicelayer.smsparser.SmsParserHelper;
+import com.jiahaoliuliu.chutoro.entity.ITransaction;
+import com.jiahaoliuliu.chutoro.entity.Transaction;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * This is a test against the sms patterns in the factory. More than a unit test
+ * this is the core system test test
+ */
+@RunWith(MockitoJUnitRunner.class)
+
+public class ADCBSmsParserParametersFactoryTest {
+
+    private static final String SOURCE = "ADCB";
+
+    // Pattern 1
+    private static final long PATTERN_1_SMS_ID = 6543;
+    private static final int PATTERN_1_SMS_QUANTITY = 359;
+    private static final long PATTERN_1_SMS_DATE = 1542443169000l;
+    private static final String PATTERN_1_SMS_DESTINATION = "GOOGLE*IDREAMS,G.CO HELPPAY#-US";
+    private static final String PATTERN_1_SMS_BODY = "Your credit card XXX4921 was used for AED3.59 on 17/11/2018 12:26:09 at "
+            + PATTERN_1_SMS_DESTINATION + ". Available credit limit is now AED9192.92.";
+
+    // Pattern 2
+    private static final long PATTERN_2_SMS_ID = 6543;
+    private static final int PATTERN_2_SMS_QUANTITY = 120000;
+    private static final long PATTERN_2_SMS_DATE = 1542346800000l;
+    private static final String PATTERN_2_SMS_DESTINATION = "ATM-MARINA MALL DXB";
+    private static final String PATTERN_2_SMS_BODY = "AED1200.00 withdrawn from acc. XXX132001 on Nov 16 2018  9:40AM at ATM-MARINA MALL DXB. Avl.Bal.AED29349.34. Be cautious with large amt. of cash.";
+
+    private ADCBSmsParserParametersFactory adcbSmsParserParametersFactory;
+    private SmsParserHelper smsParserHelper;
+
+    @Before
+    public void setup() {
+        adcbSmsParserParametersFactory = new ADCBSmsParserParametersFactory();
+        smsParserHelper = new SmsParserHelper();
+    }
+
+    @Test
+    public void testPattern1() {
+        // Prepare the data
+        Sms sms = new Sms(PATTERN_1_SMS_ID, PATTERN_1_SMS_BODY, PATTERN_1_SMS_DATE);
+
+        // Execute the method
+        List<ITransaction> transactionList =
+                smsParserHelper.mapSmsListToTransactionsList(
+                        Arrays.asList(sms),
+                        adcbSmsParserParametersFactory.createSmsParserParametersList());
+
+        // Verify the results
+        assertEquals(1, transactionList.size());
+        Transaction rightTransaction = new Transaction(PATTERN_1_SMS_ID, PATTERN_1_SMS_QUANTITY,
+                SOURCE, PATTERN_1_SMS_DESTINATION, PATTERN_1_SMS_DATE);
+        assertEquals(rightTransaction, transactionList.get(0));
+    }
+
+    @Test
+    public void testPattern2() {
+        // Prepare the data
+        Sms sms = new Sms(PATTERN_2_SMS_ID, PATTERN_2_SMS_BODY, PATTERN_2_SMS_DATE);
+
+        // Execute the method
+        List<ITransaction> transactionList =
+                smsParserHelper.mapSmsListToTransactionsList(
+                        Arrays.asList(sms),
+                        adcbSmsParserParametersFactory.createSmsParserParametersList());
+
+        // Verify the results
+        assertEquals(1, transactionList.size());
+        Transaction rightTransaction = new Transaction(PATTERN_2_SMS_ID, PATTERN_2_SMS_QUANTITY,
+                SOURCE, PATTERN_2_SMS_DESTINATION, PATTERN_2_SMS_DATE);
+        assertEquals(rightTransaction, transactionList.get(0));
+    }
+}
