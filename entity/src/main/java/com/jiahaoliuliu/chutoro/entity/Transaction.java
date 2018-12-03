3da61132377a5a8bd302ae04f974ff83127bdf0f
Jiahao Liu <jiahaoliuliu@gmail.com>
2018-12-03 19:54:21 +0400

    Using the transaction builder

--- entity/src/main/java/com/jiahaoliuliu/chutoro/entity/Transaction.java
+++ entity/src/main/java/com/jiahaoliuliu/chutoro/entity/Transaction.java
@@ -2,9 +2,9 @@ package com.jiahaoliuliu.chutoro.entity;
 
 public class Transaction implements ITransaction {
 
-    private static final long DEFAULT_SMS_ID = -1;
-    private static final int DEFAULT_QUANTITY = -1;
-    private static final long DEFAULT_DATE = -1l;
+    protected static final long DEFAULT_SMS_ID = -1;
+    protected static final int DEFAULT_QUANTITY = -1;
+    protected static final long DEFAULT_DATE = -1l;
 
     private final long smsId;
     private final int quantity;
@@ -25,147 +25,6 @@ public class Transaction implements ITransaction {
 
     public Transaction(int quantity, String currency, String source, String destination, long date) {
         this(DEFAULT_SMS_ID, quantity, currency, source, destination, date);
-    }
-
-    // TODO: Extract this to an separate class
-    /**
-     * The builder pattern for Transaction. This will allow more flexibility when the data is parsed
-     */
-    public class Builder {
-
-        // Optional
-        private long smsId = DEFAULT_SMS_ID;
-
-        // Compulsory
-        private int quantity = DEFAULT_QUANTITY;
-        private String currency;
-        private String source;
-        private String destination;
-        private long date = DEFAULT_DATE;
-
-        public Builder() {
-            this.source = source;
-            this.destination = destination;
-            this.date = date;
-        }
-
-        public Builder setSmsId(long smsId) {
-            if (smsId < 0) {
-                throw new IllegalArgumentException("The SMS id cannot be less than zero when" +
-                        "building transaction");
-            }
-
-            this.smsId = smsId;
-            return this;
-        }
-
-        public Builder setQuantity(String quantityString) {
-            try {
-                int quantity = Integer.valueOf(quantityString.trim());
-                return setQuantity(quantity);
-            } catch (NumberFormatException numberFormatException) {
-                throw new IllegalArgumentException("The quantity " + quantityString + " to be set " +
-                        "is not correct");
-            }
-        }
-
-        public Builder setQuantity(float quantityFloat) {
-            int quantity = (int) (quantityFloat * 100f);
-            return setQuantity(quantity);
-        }
-
-        /**
-         * Setting the quantity. Note that the quantity is set with two zeros, which makes the
-         * quantity multiple per 100. i.e. 123.45 is saved as 12345
-         * @param quantity
-         * @return
-         */
-        public Builder setQuantity(int quantity) {
-            if (quantity == -1) {
-                throw new IllegalArgumentException("The quantity cannot be less than zero when " +
-                    "building transaction");
-            }
-
-            this.smsId = smsId;
-            return this;
-        }
-
-        public Builder setCurrency(int currencyPosition) {
-            if (currencyPosition < 0 || currencyPosition > Currency.values().length) {
-                throw new IllegalArgumentException("The position for the currency is wrong " + currency);
-            }
-
-            return setCurrency(Currency.values()[currencyPosition]);
-        }
-
-        // This is the base method for the currency
-        public Builder setCurrency(Currency currency) {
-            this.currency = currency.toString();
-            return this;
-        }
-
-        public Builder setSource(String source) {
-            checkTextNotEmptyOrThrowIllegalArgumentException(source);
-            this.source = source;
-            return this;
-        }
-
-        public Builder setDestination(String destination) {
-            checkTextNotEmptyOrThrowIllegalArgumentException(source);
-            this.destination = destination;
-            return this;
-        }
-
-        public Builder setDate(long date) {
-            if (date < 0) {
-                throw new IllegalArgumentException("The date cannot be lower than 0 when building " +
-                        "transaction");
-            }
-            this.date = date;
-            return this;
-        }
-
-        public Transaction build() {
-            // Check the fields. For compulsory fields, if it is not filled, throw an
-            // IllegalStateException
-            // Quantity
-            if (quantity == DEFAULT_QUANTITY) {
-                throw new IllegalStateException("The quantity need to be set");
-            }
-
-            // Currency
-            checkTextNotEmptyOrThrowIllegalStateException(currency);
-
-            // Source
-            checkTextNotEmptyOrThrowIllegalStateException(source);
-
-            // Destination
-            checkTextNotEmptyOrThrowIllegalStateException(destination);
-
-            if (date == DEFAULT_DATE) {
-                throw new IllegalMonitorStateException("The date need to be set");
-            }
-
-            return new Transaction(smsId, quantity, currency, source, destination, date);
-        }
-
-        // TODO: Improve the error message
-        private boolean checkTextNotEmptyOrThrowIllegalArgumentException(String text) {
-            if (text == null || text.isEmpty()) {
-                throw new IllegalArgumentException("The text cannot be empty");
-            }
-
-            return true;
-        }
-
-        // TODO: Improve the error message
-        private boolean checkTextNotEmptyOrThrowIllegalStateException(String text) {
-            if (text == null || text.isEmpty()) {
-                throw new IllegalStateException("The text cannot be empty");
-            }
-
-            return true;
-        }
     }
 
     @Override
--- usecase/src/main/java/com/jiahaoliuliu/chutoro/usecase/AddTransactionUseCase.kt
+++ usecase/src/main/java/com/jiahaoliuliu/chutoro/usecase/AddTransactionUseCase.kt
@@ -1,57 +1,24 @@
 package com.jiahaoliuliu.chutoro.usecase
 
-import android.text.TextUtils
 import com.jiahaoliuliu.chutoro.datalayer.transactionsrepository.ITransactionsRepository
-import com.jiahaoliuliu.chutoro.entity.Transaction
+import com.jiahaoliuliu.chutoro.entity.Currency
+import com.jiahaoliuliu.chutoro.entity.TransactionBuilder
 import io.reactivex.Single
 import java.lang.IllegalArgumentException
 
 class AddTransactionUseCase(private val transactionsRepository: ITransactionsRepository) {
 
-    // TODO: Create a special class for Quantity
-    fun execute(destination: String, source: String, quantityString: String, currency: String, date: Long): Single<Boolean> {
+    fun execute(destination: String, source: String, quantity: String, currency: Currency, date: Long): Single<Boolean> {
         try {
-            val transaction = Transaction.Builder()
+            val transaction = TransactionBuilder(source, destination, date)
+                    .setQuantity(quantity)
+                    .setCurrency(currency)
+                    .setDate(date)
+                    .build()
+            return transactionsRepository.addTransaction(transaction)
+        } catch (illegalArgumentException: IllegalArgumentException) {
+            return Single.error(illegalArgumentException)
         }
-//        if (isAllDataValid(destination, source, quantityString, currency, date)) {
-//            // Quantity
-//            val quantity = parseQuantity(quantityString)
-//
-//            val transaction = Transaction(quantity, currency, source, destination, date)
-//            return transactionsRepository.addTransaction(transaction)
-//        } else {
-//            return Single.error(IllegalArgumentException("The parameters are not correct"))
-//        }
-    }
-
-    fun parseQuantity(quantityString: String): Int {
-        return (quantityString.toFloat() * 100.0).toInt()
-    }
-
-    private fun isAllDataValid(destination: String, source: String, quantity: String,
-                               currency: String, date: Long): Boolean {
-        return isDestinationValid(destination) && isSourceValid(source) &&
-                isQuantityValid(quantity) && isCurrencyValid(currency) && isDateValid(date)
-    }
-
-    private fun isDestinationValid(destination: String): Boolean {
-        return !TextUtils.isEmpty(destination)
-    }
-
-    private fun isSourceValid(source: String): Boolean {
-        return !TextUtils.isEmpty(source)
-    }
-
-    private fun isQuantityValid(quantity: String): Boolean {
-        return quantity.toFloatOrNull() != null
-    }
-
-    private fun isCurrencyValid(currency: String): Boolean {
-        return !TextUtils.isEmpty(currency)
-    }
-
-    private fun isDateValid(date: Long): Boolean {
-        return date > 0
     }
 
 }
