ca18e591f94159baa467700d520dbf0bd73cdfc6
Jiahao Liu <jiahaoliuliu@gmail.com>
2018-12-09 19:10:25 +0400

    Improved the currency spinner

--- app/src/main/java/com/jiahaoliuliu/chutoro/ui/addtransaction/AddTransactionActivity.kt
+++ app/src/main/java/com/jiahaoliuliu/chutoro/ui/addtransaction/AddTransactionActivity.kt
@@ -13,8 +13,11 @@ import kotlinx.android.synthetic.main.activity_add_transaction.*
 import java.lang.IllegalArgumentException
 import java.util.*
 import javax.inject.Inject
+import android.widget.AdapterView
+import android.widget.Toast
+import android.widget.AdapterView.OnItemSelectedListener
 
-class AddTransactionActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener,
+class AddTransactionActivity : AppCompatActivity(), //AdapterView.OnItemSelectedListener,
         AddTransactionContract.View {
     @Inject
     lateinit var presenter: AddTransactionContract.Presenter
@@ -46,7 +49,20 @@ class AddTransactionActivity : AppCompatActivity(), AdapterView.OnItemSelectedLi
             // Apply the adapter to the spinner
             currencySpinner.adapter = adapter
         }
-        currencySpinner.onItemSelectedListener = this
+
+        currencySpinner.onItemSelectedListener = object : OnItemSelectedListener {
+
+            override fun onItemSelected(adapterView: AdapterView<*>, view: View,
+                                        position: Int, id: Long) {
+                if (position >= 0 && position < Currency.values().size) {
+                    currency = Currency.values()[position]
+                }
+            }
+
+            override fun onNothingSelected(adapterView: AdapterView<*>) {
+                currency = defaultCurrency
+            }
+        }
 
         val addTransactionButton = findViewById<Button>(R.id.add)
         addTransactionButton.setOnClickListener { _ ->
@@ -58,17 +74,6 @@ class AddTransactionActivity : AppCompatActivity(), AdapterView.OnItemSelectedLi
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setDisplayShowHomeEnabled(true)
         title = getString(R.string.add_transaction_title)
-    }
-
-    // Spinner
-    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-        if (position >= 0 && position < Currency.values().size) {
-            currency = Currency.values()[position]
-        }
-    }
-
-    override fun onNothingSelected(parent: AdapterView<*>?) {
-        currency = defaultCurrency
     }
 
     override fun onInsertionCorrect() {
