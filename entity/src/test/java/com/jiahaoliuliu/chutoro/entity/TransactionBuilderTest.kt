1df11215740d76cd863105c0be38ccf2c62e8492
Jiahao Liu <jiahaoliuliu@gmail.com>
2018-12-09 19:51:49 +0400

    Created the unit tests for TransactionBuilder

--- entity/src/test/java/com/jiahaoliuliu/chutoro/entity/TransactionBuilderTest.kt
+++ entity/src/test/java/com/jiahaoliuliu/chutoro/entity/TransactionBuilderTest.kt
@@ -5,6 +5,7 @@ import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.junit.MockitoJUnitRunner
+import java.lang.IllegalArgumentException
 import java.lang.IllegalStateException
 
 @RunWith(MockitoJUnitRunner::class)
@@ -123,4 +124,149 @@ class TransactionBuilderTest {
         assertEquals(transactionExpected, transactionCreated)
     }
 
+    @Test(expected = IllegalArgumentException::class)
+    fun testBuild_SetWrongSms() {
+        // Prepare the data
+        val wrongSmsId = -11L
+        transactionBuilder.setSmsId(wrongSmsId)
+
+        // Invoke the method
+        val transactionCreated = transactionBuilder.build()
+
+        // Check the result
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun testBuild_SetWrongQuantityAsString() {
+        // Prepare the data
+        transactionBuilder.setSmsId(SMS_ID)
+        val wrongQuantity = "wrongQuantity"
+        transactionBuilder.setQuantity(wrongQuantity)
+
+        // Invoke the method
+        val transactionCreated = transactionBuilder.build()
+
+        // Check the result
+    }
+
+    @Test
+    fun testBuild_SetQuantityAsFloat() {
+        // Prepare the data
+        transactionBuilder.setSmsId(SMS_ID)
+        val quantityFloat = QUANTITY / 100f
+        transactionBuilder.setQuantity(quantityFloat)
+        transactionBuilder.setCurrency(CURRENCY_AED)
+        transactionBuilder.setSource(SOURCE_ADCB)
+        transactionBuilder.setDestination(DESTINATION)
+        transactionBuilder.setDate(DATE)
+        val transactionExpected = Transaction(SMS_ID, QUANTITY, CURRENCY_AED, SOURCE_ADCB,
+                DESTINATION, DATE)
+
+        // Invoke the method
+        val transactionCreated = transactionBuilder.build()
+
+        // Check the result
+        assertEquals(transactionExpected, transactionCreated)
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun testBuild_SetWrongQuantityAsInt() {
+        // Prepare the data
+        transactionBuilder.setSmsId(SMS_ID)
+        val wrongQuantity = -1
+        transactionBuilder.setQuantity(wrongQuantity)
+
+        // Invoke the method
+        val transactionCreated = transactionBuilder.build()
+
+        // Check the result
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun testBuild_SetEmptyCurrency() {
+        // Prepare the data
+        transactionBuilder.setSmsId(SMS_ID)
+        transactionBuilder.setQuantity(QUANTITY)
+        transactionBuilder.setCurrency("")
+
+        // Invoke the method
+        val transactionCreated = transactionBuilder.build()
+
+        // Check the result
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun testBuild_SetWrongCurrency() {
+        // Prepare the data
+        transactionBuilder.setSmsId(SMS_ID)
+        transactionBuilder.setQuantity(QUANTITY)
+        val wrongCurrency = "adabsdf"
+        transactionBuilder.setCurrency(wrongCurrency)
+
+        // Invoke the method
+        val transactionCreated = transactionBuilder.build()
+
+        // Check the result
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun testBuild_SetEmptySource() {
+        // Prepare the data
+        transactionBuilder.setSmsId(SMS_ID)
+        transactionBuilder.setQuantity(QUANTITY)
+        transactionBuilder.setCurrency(CURRENCY_AED)
+        transactionBuilder.setSource("")
+
+        // Invoke the method
+        val transactionCreated = transactionBuilder.build()
+
+        // Check the result
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun testBuild_SetWrongSource() {
+        // Prepare the data
+        transactionBuilder.setSmsId(SMS_ID)
+        transactionBuilder.setQuantity(QUANTITY)
+        transactionBuilder.setCurrency(CURRENCY_AED)
+        val wrongSource = "asdfadsf"
+        transactionBuilder.setSource(wrongSource)
+
+        // Invoke the method
+        val transactionCreated = transactionBuilder.build()
+
+        // Check the result
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun testBuild_SetEmptyDestination() {
+        // Prepare the data
+        transactionBuilder.setSmsId(SMS_ID)
+        transactionBuilder.setQuantity(QUANTITY)
+        transactionBuilder.setCurrency(CURRENCY_AED)
+        transactionBuilder.setSource(SOURCE_ADCB)
+        transactionBuilder.setDestination("")
+
+        // Invoke the method
+        val transactionCreated = transactionBuilder.build()
+
+        // Check the result
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun testBuild_SetWrongDate() {
+        // Prepare the data
+        transactionBuilder.setSmsId(SMS_ID)
+        transactionBuilder.setQuantity(QUANTITY)
+        transactionBuilder.setCurrency(CURRENCY_AED)
+        transactionBuilder.setSource(SOURCE_ADCB)
+        transactionBuilder.setDestination(DESTINATION)
+        val wrongDate = -1L
+        transactionBuilder.setDate(wrongDate)
+
+        // Invoke the method
+        val transactionCreated = transactionBuilder.build()
+
+        // Check the result
+    }
 }
