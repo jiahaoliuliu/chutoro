d5dd6e5baa5b83eef335a59316f42e58a5e428cf
Jiahao Liu <jiahao.liu@dnata.com>
2018-12-18 13:22:45 +0400

    Improved the pattern

--- devicelayer/src/main/java/com/jiahaoliuliu/chutoro/devicelayer/smsparser/smsparserparameters/EmiratesNBDParametersFactory.java
+++ devicelayer/src/main/java/com/jiahaoliuliu/chutoro/devicelayer/smsparser/smsparserparameters/EmiratesNBDParametersFactory.java
@@ -13,7 +13,7 @@ public class EmiratesNBDParametersFactory implements ISmsParametersFactory {
     // Pattern 1
     // Example:
     // Purchase of AED 26.75 with Debit Card ending 6819 at ZOOM SITE 6562 BURJ AL SA, DUBAI. Avl Balance is AED 54,520.42.
-    private static final String PATTERN_1 = "Purchase of (.*?) with (.*) ending (.*) at (.*?). Avl Balance is (.*?).";
+    private static final String PATTERN_1 = "Purchase of (.*?) with (.*) ending (.*) at (.*?)\\.";
     private static final int PATTERN_1_POSITION_QUANTITY = 1;
     private static final int PATTERN_1_POSITION_DESTINATION = 4;
     // The date is not shown on the sms. In this case, the received date of the sms will be used
