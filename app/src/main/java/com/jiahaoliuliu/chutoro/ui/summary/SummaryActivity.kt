aaf4820c139a6f2fa4c7e0980ade181f83af90e2
Jiahao Liu <jiahaoliuliu@gmail.com>
2019-01-10 18:01:25 +0400

    Created the empty Summary activity

---
+++ app/src/main/java/com/jiahaoliuliu/chutoro/ui/summary/SummaryActivity.kt
@@ -0,0 +1,13 @@
+package com.jiahaoliuliu.chutoro.ui.summary
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.jiahaoliuliu.chutoro.R
+
+class SummaryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_summary)
+    }
+}
